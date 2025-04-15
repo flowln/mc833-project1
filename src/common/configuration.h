443f751171d6a0cc9f54e2b4c1814020c339a913
Sofia Donato Ferreira <s194300@dac.unicamp.br>
2025-04-15 20:56:09 -0300

    refactor: add some documentation about parameters in configuration.h
    Signed-off-by: Sofia Donato Ferreira <s194300@dac.unicamp.br>

--- src/common/configuration.h
+++ src/common/configuration.h
@@ -1,4 +1,7 @@
+/* IP address where the server is running. This is used by the client to establish a connection. */
 #define SERVER_LISTEN_HOST "127.0.0.1"
+/* TCP port where the server will be listening for new connections. */
 #define SERVER_LISTEN_PORT 61623
 
+/* Location relative to the CWD where the SQLite file will be. */
 #define SERVER_DB_LOCATION "catalog.db"
